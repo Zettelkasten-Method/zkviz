4fffa8247de31efe01191c94bac5ea9481cd2129
Alexandre Chabot-Leclerc <alex@alexchabot.net>
2019-06-09 14:44:22 -0500

    ENH: Add cli entry point

--- setup.py
+++ setup.py
@@ -26,4 +26,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    entry_points={
+        'console_scripts': [
+            'zkviz = zkviz.zkviz:main'
+        ]
+    }
 )
